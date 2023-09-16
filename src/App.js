f166c1cc31b92fdd5f6bbc9bd6354ed572c274ca
Fuyad <fuyad@gmail.com>
2023-09-16 14:16:43 +0600

    this is my responsive-navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleShuffle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="flex justify-between items-center mx-[100px] py-4">
+      <div>
+        <h1 className="text-4xl font-bold">Logo</h1>
+      </div>
+
+      <ul className="hidden md:flex justify-between items-center w-[600px] text-md font-medium">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/news">News</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/Carrier">Carrier</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+
+      <button onClick={handleShuffle} className="text-xl">
+        {isOpen ? (
+          <FontAwesomeIcon icon={faXmark} />
+        ) : (
+          <FontAwesomeIcon icon={faBars} className="md:hidden" />
+        )}
+
+        {isOpen && (
+          <ul
+            className={
+              isOpen
+                ? "fixed left-0 top-20 w-[40%] text-md font-medium shadow-lg py-6 h-full border rounded-e-xl slide-in"
+                : "slide-out w-0"
+            }
+          >
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/news">News</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/Carrier">Carrier</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        )}
+      </button>
+    </nav>
   );
-}
+};
 
 export default App;
